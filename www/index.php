6920bf474428e8cd5a80a794dedfab07f34f9eeb
drodriguez357 <drodriguez1109@alumnes.politecnicllevant.cat>
2024-10-07 10:37:33 +0200

    Mi Primer Commit

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>Document</title>
+  <meta name="keywords" content=""/>
+  <meta name="description" content=""/>
+  <meta name="viewport" content="width=device-width"/>
+</head>
+<body>
+<h1><?php
+  echo "Hola mundo en php!";
+?></h1>
+</body>
+</html> 
